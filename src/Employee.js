9da31a049f6ee46f422ce9e76c39c18396f0bd4d
Dmitrii Ermolaev <bugiman532@gmail.com>
2023-03-18 18:48:36 +0300

    create components/, replace all component to components/, change files extensions of components,  delete junk file, edit import command in index.jsx

--- src/Employee.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function Employee({surname,name, patronymic, salary}){
-    return <div>
-        <p>{surname}-{name}-{patronymic}-{salary}</p>
-    </div>
-}
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "../src/index.css"
-import CheckList from "./CheckList"
+import CheckList from "./components/CheckList"
 
 
 function App() {
